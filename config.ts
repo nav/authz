f92569b3247513cb1a55151282ca61e57a8f6d5f
Nav Aulakh <nav@users.noreply.github.com>
2021-09-08 16:05:47 -0700

    Fixed build issues (#4)

--- config.ts
+++ config.ts
@@ -2,9 +2,10 @@ const definitions = {
   API_REQUEST_HEADERS: {
     Authorization: `Token ${process.env.ACCESS_TOKEN}`,
   },
-  API_SERVER: process.env.API_SERVER,
-  SEGMENT_NAME: process.env.NEXT_PUBLIC_SEGMENT_NAME,
-  SEGMENT_NAME_PLURAL: process.env.NEXT_PUBLIC_SEGMENT_NAME_PLURAL,
+  API_SERVER: process.env.API_SERVER || "http://localhost:3000",
+  SEGMENT_NAME: process.env.NEXT_PUBLIC_SEGMENT_NAME || "Segment",
+  SEGMENT_NAME_PLURAL:
+    process.env.NEXT_PUBLIC_SEGMENT_NAME_PLURAL || "Segments",
 };
 
 export { definitions };
