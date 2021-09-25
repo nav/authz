e138c6f493011661b3f50e410e7d698f10711a1f
Nav Aulakh <nav@users.noreply.github.com>
2021-09-24 21:51:55 -0700

    Fixed API server env var (#5)

--- config.ts
+++ config.ts
@@ -2,7 +2,7 @@ const definitions = {
   API_REQUEST_HEADERS: {
     Authorization: `Token ${process.env.ACCESS_TOKEN}`,
   },
-  API_SERVER: process.env.API_SERVER || "http://localhost:3000",
+  API_SERVER: process.env.NEXT_PUBLIC_API_SERVER || "http://localhost:3000",
   SEGMENT_NAME: process.env.NEXT_PUBLIC_SEGMENT_NAME || "Segment",
   SEGMENT_NAME_PLURAL:
     process.env.NEXT_PUBLIC_SEGMENT_NAME_PLURAL || "Segments",
